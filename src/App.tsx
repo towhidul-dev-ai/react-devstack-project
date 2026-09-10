1d609c4e54e637e69544262d0e64bf4df4805687
towhidulislamshahed25 <towhidulislamshahed1@gmail.com>
2026-09-10 20:53:23 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <h4 className="text-6xl bg-amber-500">DevStack Project</h4>
+      <button className="btn btn-success">Success</button>
+    </>
+  )
+}
+
+export default App
